576a5b4fd93ef8029979203eea64a191cbba1ebd
tuidienanhvan <913.tuanhvan2018@gmail.com>
2026-01-19 14:10:20 +0700

    replace old repo with local code

--- src/features/game/events/pranks/system-error/config.ts
+++ src/features/game/events/pranks/system-error/config.ts
@@ -4,9 +4,9 @@ import { GameEvent } from '../../core/types';
 export const SYSTEM_ERROR_EVENT: GameEvent = {
     id: 'system_error',
     type: 'funny',
-    trigger: { level: [5, 10, 15], probability: 0.001, condition: 'on_start' },
+    trigger: { level: [5, 10], probability: 0.001, condition: 'on_start' },
     message: 'Oops! Mất câu hỏi rồi...',
-    duration: 40000,
+    duration: 3000,
     showOverlay: false,
     afterEffect: 'show_real_question'
 };
